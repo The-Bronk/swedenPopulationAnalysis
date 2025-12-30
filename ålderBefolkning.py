5a2492eceac11ffcfc25ed6ea3fde9377b077495
The-Bronk <jesper.christoffersson@hotmail.com>
2025-12-30 21:25:34 +0100

    Deleted swedish populationAge file

--- ålderBefolkning.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-populationWithAge = pd.read_csv("/home/jesper/sweden-population-analysis/rawData/TAB5890_sv.csv", encoding= 'Latin1')
-
-print(populationWithAge.head)
-print(populationWithAge.tail)
-
